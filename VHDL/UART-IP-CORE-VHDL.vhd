277f7c79936d405cee0b2dffec5ee3d1b854c950
ARMontt <alan.r.montt@outlook.com>
2025-08-09 23:24:24 -0400

    Added VHDL Files

---
+++ VHDL/UART-IP-CORE-VHDL.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.Std_Logic_1164.all;
+
+entity UART-IP-CORE-VHDL is
+
+end UART-IP-CORE-VHDL;
+
+architecture BEHAV of UART-IP-CORE-VHDL is
+--	BEGIN Signals declaration	--
+
+
+--	END Signals declaration		--
+begin 
+--Concurrential
+
+end BEHAV;
