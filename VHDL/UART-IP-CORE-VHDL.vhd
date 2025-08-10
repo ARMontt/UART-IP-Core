716b66d8e478cf6bdc946f74e6d90ad1340e106c
ARMontt <alan.r.montt@outlook.com>
2025-08-10 03:24:45 -0400

    APB File started

--- VHDL/UART-IP-CORE-VHDL.vhd
+++ VHDL/UART-IP-CORE-VHDL.vhd
@@ -2,12 +2,20 @@ library IEEE;
 use IEEE.Std_Logic_1164.all;
 
 entity UART-IP-CORE-VHDL is
-
+	port(
+		clk_in	:	in 	std_logic;
+		rx			:	in 	std_logic;
+		tx			:	out 	std_logic;
+		
+		data		:	in		std_logic;
+		start		:	in		std_logic;
+		
+	);
 end UART-IP-CORE-VHDL;
 
 architecture BEHAV of UART-IP-CORE-VHDL is
 --	BEGIN Signals declaration	--
-
+signal 
 
 --	END Signals declaration		--
 begin 
